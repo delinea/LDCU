b12d332c13858c2c6b130bb531daa856f240e4e8
Adrien Deline <adrien.deline@unige.ch>
2024-02-22 14:49:40 +0100

    minor correction

--- get_lds_with_errors_v3.py
+++ get_lds_with_errors_v3.py
@@ -414,7 +414,6 @@ def get_subgrids(Teff=(-np.inf, np.inf), logg=(-np.inf, np.inf),
                 "PHOENIX": os.path.join(PHOENIX_DIR, "phoenix_grid.fits.gz")}
     for model, grid_fn in grid_fns.items():
         if not os.path.isfile(grid_fn):
-            print(grid_fn)
             raise FileNotFoundError("{} grid file could not be found: "
                                     "'update_{}_grid()' must be called first"
                                     .format(model, model.lower()))
